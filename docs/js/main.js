dc94b5a0710ae6e9f4ce9a5a0371348e4c24789e
Dylan Navas <dylan2018.2019@gmail.com>
2019-07-08 03:08:07 -0300

    Dark Mode JS and other changes...

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,25 @@
-const darkmode = () => document.body.classList.toggle("dark-container");
+var tempBack;
+var tempText;
+var tempBubble;
+var darkMode = false;
+
+const darkmode = () => {
+    let style = getComputedStyle(document.body);
+    let root = document.documentElement;
+
+    if (darkMode) {
+        root.style.setProperty('--color-background', tempBack);
+        root.style.setProperty('--color-text', tempText);
+        root.style.setProperty('--color-bubble', tempBubble);
+    } else {
+        tempBack = style.getPropertyValue('--color-background');
+        tempText = style.getPropertyValue('--color-text');
+        tempBubble = style.getPropertyValue('--color-bubble');
+
+        root.style.setProperty('--color-background', "#222020");
+        root.style.setProperty('--color-text', "#f5f5f5");
+        root.style.setProperty('--color-bubble', "#555");
+    }
+
+    darkMode = !darkMode;
+};
