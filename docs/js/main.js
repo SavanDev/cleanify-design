8b5f7d9380227214dc5ac674155a9c6f9c68e455
Dylan Navas <dylan2018.2019@gmail.com>
2019-07-09 05:54:14 -0300

    Dirty implementation of Bootstrap Grid system

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,24 +1,27 @@
-var tempBack;
-var tempText;
-var tempBubble;
+let style = getComputedStyle(document.body);
+
+var tempBack = style.getPropertyValue('--color-background');
+var tempText = style.getPropertyValue('--color-text');
+var tempCont = style.getPropertyValue('--color-container');
+var tempBackText = style.getPropertyValue('--color-back-text');
+var tempButton = style.getPropertyValue('--color-button');
 var darkMode = false;
 
 const darkmode = () => {
-    let style = getComputedStyle(document.body);
     let root = document.documentElement;
 
     if (darkMode) {
         root.style.setProperty('--color-background', tempBack);
         root.style.setProperty('--color-text', tempText);
-        root.style.setProperty('--color-bubble', tempBubble);
+        root.style.setProperty('--color-container', tempCont);
+        root.style.setProperty('--color-back-text', tempBackText);
+        root.style.setProperty('--color-button', tempButton);
     } else {
-        tempBack = style.getPropertyValue('--color-background');
-        tempText = style.getPropertyValue('--color-text');
-        tempBubble = style.getPropertyValue('--color-bubble');
-
         root.style.setProperty('--color-background', "#222020");
         root.style.setProperty('--color-text', "#f5f5f5");
-        root.style.setProperty('--color-bubble', "#555");
+        root.style.setProperty('--color-container', "#555");
+        root.style.setProperty('--color-back-text', "#555");
+        root.style.setProperty('--color-button', "#555");
     }
 
     darkMode = !darkMode;
